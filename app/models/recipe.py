a115dfab38d768964cf37452b2a9cdc28d6c20f8
Anjula Tamang <parkhrinj@gmail.com>
2023-01-26 22:42:21 -0500

    changed r/s to backref instead of backpopulates

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -4,6 +4,6 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ingredients =db.Column(db.Array(db.String))
     instruction = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     user= db.relationship("User", back_populates="recipes")
 
--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     # username 
     email = db.Column(db.String)
-    recipes = db.relationship("Recipe", back_populates="user")
+    recipes = db.relationship("Recipe", backref="recipe", lazy=True)
     # leave off for now- what this means if the page is refreshed user has to input ingredients in again
     # if we add, we'll need to write additional code to save each ingredient with the ability for the user to add more in the future.
     # ingredients =db.Column(db.Array(db.String))# Column("data",ARRAY(String))
